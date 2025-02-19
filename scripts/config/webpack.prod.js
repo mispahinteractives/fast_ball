1ba04b569049a3e904da201de3596ed56ff307eb
mispah interactives <mispahinteractives@gmail.com>
2025-02-19 13:21:39 +0530

    fixed bundle js error

---
+++ scripts/config/webpack.prod.js
@@ -0,0 +1,19 @@
+const path = require("path");
+
+module.exports = {
+    mode: "production",
+    entry: path.resolve(__dirname, "../../main.js"),
+    output: {
+        path: path.resolve(__dirname, "../../dist"),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: "babel-loader"
+            }
+        }]
+    }
+};
